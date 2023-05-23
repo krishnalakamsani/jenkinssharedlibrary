780ad352a45a0f294e5f90dacb78bc23e9914c40
krishnalakamsani <124685634+krishnalakamsani@users.noreply.github.com>
2023-05-24 02:08:41 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -1,4 +1,4 @@
-def call (String gitBranch = 'main', String gitUrl = 'https://github.com/krishnalakamsani/fleetapp.git', String imageName = 'krishnalakamsani/fleetappapi', String path = 'sourcecode/k8s-fleetman-api-gateway') {
+def call (String gitBranch = 'main', String gitUrl = 'https://github.com/krishnalakamsani/fleetapp.git', String imageName = 'krishnalakamsani/fleetappapi', String path = '/home/ubuntu/jenkins/workspace/fleetapp-k8s-fleetman-api-gateway/sourcecode/k8s-fleetman-api-gateway') {
 pipeline {
   agent {
       label 'docker'
