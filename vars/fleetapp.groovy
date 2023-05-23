97a551fd3184b2a8278332c0711b2a8804cd1d1a
krishnalakamsani <124685634+krishnalakamsani@users.noreply.github.com>
2023-05-24 02:13:20 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -11,7 +11,7 @@ pipeline {
     }
     stage('Build') {
       steps {
-        sh 'cd sourcecode/k8s-fleetman-api-gateway'
+        sh 'cd sourcecode/k8s-fleetman-api-gateway/'
         sh 'mvn clean package'
         }
     }
