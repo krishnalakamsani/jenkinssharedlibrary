893b4afb29d44a3d0fefebf0a99f33aad896355a
krishnalakamsani <124685634+krishnalakamsani@users.noreply.github.com>
2023-05-24 00:24:33 +0530

    Create fleetapp.groovy

---
+++ vars/fleetapp.groovy
@@ -0,0 +1,29 @@
+def call (String label = 'a', String gitBranch = 'a', String gitUrl = 'a', String imageName = 'a', String dockerhubCred = 'a', String path = 'a')
+pipeline {
+  agent {
+      label '$label'
+    }
+    environment {
+		LOGIN_CREDENTIALS=credentials('$dockerhubCred')
+	}
+  stages {
+    stage('Checkout') {
+      steps {
+        git branch: '$gitBranch', url: '$gitUrl'
+        }
+    }
+    stage('Build') {
+      steps {
+        sh 'cd $path'
+        sh 'mvn clean package'
+        }
+    }
+    stage('Build and Push Docker Image') {
+      steps {
+        sh 'docker build -t $imageName:$BUILD_NUMBER .'
+        sh 'echo $LOGIN_CREDENTIALS_PSW | docker login -u $LOGIN_CREDENTIALS_USR --password-stdin'
+        sh 'docker push $imageName:$BUILD_NUMBER'
+        }
+    }
+  }
+}
