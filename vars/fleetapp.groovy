fe4e71d1210b4e7b6ae2ad758007a41ed748229e
krishnalakamsani <124685634+krishnalakamsani@users.noreply.github.com>
2023-05-24 02:28:00 +0530

    Update fleetapp.groovy

--- vars/fleetapp.groovy
+++ vars/fleetapp.groovy
@@ -1,26 +1,25 @@
-def call (String gitBranch = 'main', String gitUrl = 'https://github.com/krishnalakamsani/fleetapp.git', String imageName = 'krishnalakamsani/fleetappapi', String path = '/home/ubuntu/jenkins/workspace/fleetapp-k8s-fleetman-api-gateway/sourcecode/k8s-fleetman-api-gateway') {
-pipeline {
-  agent {
+def call(String gitBranch = 'main', String gitUrl = 'https://github.com/krishnalakamsani/fleetapp.git', String imageName = 'krishnalakamsani/fleetappapi', String path = '/home/ubuntu/jenkins/workspace/fleetapp-k8s-fleetman-api-gateway/sourcecode/k8s-fleetman-api-gateway') {
+  pipeline {
+    agent {
       label 'docker'
     }
-     stages {
-    stage('Checkout') {
-      steps {
-        git branch: 'main', url: 'https://github.com/krishnalakamsani/fleetapp.git'
+    stages {
+      stage('Checkout') {
+        steps {
+          git branch: gitBranch, url: gitUrl
         }
-    }
-    stage('Build') {
-      steps {
-        sh 'cd sourcecode/k8s-fleetman-api-gateway/'
-        sh 'mvn clean package'
+      }
+      stage('Build') {
+        steps {
+          sh "cd '${path}' && mvn clean package"
         }
-    }
-    stage('Build and Push Docker Image') {
-      steps {
-        sh 'docker build -t $imageName:$BUILD_NUMBER .'
-        sh 'docker push $imageName:$BUILD_NUMBER'
+      }
+      stage('Build and Push Docker Image') {
+        steps {
+          sh "docker build -t ${imageName}:${BUILD_NUMBER} ."
+          sh "docker push ${imageName}:${BUILD_NUMBER}"
         }
+      }
     }
   }
-}
 }
